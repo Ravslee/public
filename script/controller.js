bca316474464757e2d2fd90b918bd15f1eb1d2e2
Ravslee <ravi.rr@hotmail.com>
2017-01-22 18:14:31 +0530

    SPA and Social platform integration
    SPA is fully done , while Social platform integration comments /like
    done ompletely.
    while Share on fb require a directive , which we will desgin in next
    branch,
    Comiting the above mentioned changes to DCP,
    Thank you!

--- script/controller.js
+++ script/controller.js
@@ -3,16 +3,13 @@ var module = angular.module("DCP",['firebase','ngRoute']);
 module.config(
 function($routeProvider){
     $routeProvider
-
-
      .when('/',{
         templateUrl:"home.html",
         controller:"blog"
     })
-    
-    .when('/blogs',{
-        templateUrl:"blogs.html",
-        
+    .when('/content/:blogId/:headline',{
+        templateUrl:"/pages/content.html",
+        controller:"blog"
     });
    
 
@@ -23,6 +20,8 @@ function($routeProvider){
 
 
 module.controller('blog',function($firebaseObject,$routeParams){
+    this.blogId= $routeParams.blogId;
+this.headline= $routeParams.headline;
    var self = this;
     const rootRef = firebase.database().ref().child('blogs');
     this.rootReference = $firebaseObject(rootRef);
@@ -32,7 +31,36 @@ module.controller('blog',function($firebaseObject,$routeParams){
  console.log("loaded record");
 self.show= true;
     });
-this.blogId= $routeParams.blogId;
-this.headline= $routeParams.headline;
-    
+
+
+
+
+});
+
+
+module.directive('dynFbCommentBox', function () {
+    function createHTML(href, numposts, colorscheme, width) {
+        return '<div class="fb-comments" ' +
+                       'data-href="' + href + '" ' +
+                       'data-numposts="' + numposts + '" ' +
+                       'data-colorsheme="' + colorscheme + '" ' +
+                       'data-width="' + width + '">' +
+               '</div>';
+    }
+
+
+    return {
+        restrict: 'A',
+        scope: {},
+        link: function postLink(scope, elem, attrs) {
+            attrs.$observe('pageHref', function (newValue) {
+                var href        = newValue;
+                var numposts    = attrs.numposts    || 5;
+                var colorscheme = attrs.colorscheme || 'light';
+                var width = attrs.width || '100%';
+                elem.html(createHTML(href, numposts, colorscheme, width));
+                FB.XFBML.parse(elem[0]);
+            });
+        }
+    };
 });
